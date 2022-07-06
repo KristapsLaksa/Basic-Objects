cd0ae40ad997e9d9370b5af1d9c24db6e6e1ea98
KristapsLaksa <103658142+KristapsLaksa@users.noreply.github.com>
2022-07-06 19:21:43 +0300

    Add files via upload

--- 10.php
+++ 10.php
@@ -1,25 +1,27 @@
 <?php
 
-class Application{
+class Application
+{
     private VideoStore $store;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->store = new VideoStore();
     }
 
-    public function run():void
+    public function run(): void
     {
-        while (true){
-            echo "Choose the option \n";
-            echo "Choose 0 to EXIT \n";
-            echo "Choose 1 to fill video store \n";
-            echo "Choose 2 to rent a video \n";
-            echo "Choose 3 to return video \n";
-            echo "choose 4 to list inventory \n";
+        while (true) {
+            echo "Choose the operation you want to perform \n";
+            echo "Choose 0 for EXIT\n";
+            echo "Choose 1 to fill video store\n";
+            echo "Choose 2 to rent video (as user)\n";
+            echo "Choose 3 to return video (as user)\n";
+            echo "Choose 4 to list inventory\n";
 
             $command = (int)readline();
 
-            switch ($command){
+            switch ($command) {
                 case 0:
                     echo "Bye!";
                     die;
@@ -28,103 +30,133 @@ class Application{
                     break;
                 case 2:
                     $this->rentVideo();
+                    break;
                 case 3:
                     $this->returnVideo();
+                    break;
                 case 4:
                     $this->listInventory($this->store->getAvailableVideos());
                     break;
                 default:
-                    echo "Wrong command!";
+                    echo "Sorry, I don't understand you..";
             }
         }
     }
-    private function addVideo():void{
-        $movieTitle=readline('Enter movie title: ');
+
+    private function addVideo(): void
+    {
+        $movieTitle = readline('Enter movie title: ');
         $this->store->add(new Video($movieTitle));
     }
-    private function rentVideo():void{
-        $this->listInventory($this->store->getAvailableVideos());
 
-        $inventory = $this->store->getAvailableVideos();
+    private function rentVideo(): void
+    {
 
-        $selectedNumber = (int) readline('Enter movie number : ');
+        $availableVideos = $this->store->getAvailableVideos();
 
-        $selectedVideo =$inventory[$selectedNumber];
+        $this->listInventory($availableVideos);
+
+        $selectedNumber = (int)readline('Enter movie number: ');
+
+        $selectedVideo = $availableVideos[$selectedNumber];
 
         $this->store->rentVideo($selectedVideo);
     }
-    private function returnVideo():void{
+
+    private function returnVideo(): void
+    {
         $this->listInventory($this->store->getRentedVideos());
         $inventory = $this->store->getRentedVideos();
 
-        if(empty($inventory)) return;
-        $selectedNumber = (int) readline('Enter movie number : ');
+        if (empty($inventory)) return;
+
+        $selectedNumber = (int)readline('Enter movie number: ');
+
+
         $selectedVideo = $inventory[$selectedNumber];
+
         $this->store->returnVideo($selectedVideo);
+
         $rating = (float)readline('Enter rating: ');
+
         $selectedVideo->receiveRating($rating);
     }
-    private function listInventory(array $videos):void{
-        foreach($videos as $number=>$video){
-            echo "[{$number}] - {$video->getTitle()} / {$video->averageRating()}".PHP_EOL;
+
+    private function listInventory(array $videos): void
+    {
+        foreach ($videos as $number => $video) {
+            echo "[$number] - {$video->getTitle()} / {$video->averageRating()}" . PHP_EOL;
         }
     }
 }
 
+class VideoStore
+{
+    private array $availableVideos = [];
+    private array $rentedVideos = [];
 
-class VideoStore {
-private array $availableVideos = [];
-private array $rentedVideos = [];
+    public function add(Video $video): void
+    {
+        $this->availableVideos[] = $video;
+    }
 
-public function add(Video $video):void{
-    $this->availableVideos[]=$video;
-}
-public function rentVideo(Video $video):void{
-    if(($key = array_search($video,$this->availableVideos))!== false){
-        unset($this->availableVideos[$key]);
-        $this->rentedVideos[]=$video;
+    public function rentVideo(Video $video): void
+    {
+        if (($key = array_search($video, $this->availableVideos)) !== false) {
+            unset($this->availableVideos[$key]);
+            $this->rentedVideos[] = $video;
+        }
+
     }
-}
-public function returnVideo(Video $video):void{
-    if(($key = array_search($video,$this->rentedVideos))!== false){
-        unset($this->rentedVideos[$key]);
-        $this->availableVideos[]=$video;
+
+    public function returnVideo(Video $video): void
+    {
+        if (($key = array_search($video, $this->rentedVideos)) !== false) {
+            unset($this->rentedVideos[$key]);
+            $this->availableVideos[] = $video;
+        }
+
     }
-    }
-public function getAvailableVideos(): array
+
+
+    public function getAvailableVideos(): array
     {
         return $this->availableVideos;
     }
 
-
-public function getRentedVideos(): array
+    public function getRentedVideos(): array
     {
         return $this->rentedVideos;
     }
-
 }
 
-class Video {
+class Video
+{
     private string $title;
     private array $ratings = [];
 
-    public function __construct(string $title){
-        $this->title=$title;
-
+    public function __construct(string $title)
+    {
+        $this->title = $title;
     }
 
     public function getTitle(): string
     {
         return $this->title;
     }
-    public function receiveRating(float $rating){
-        $this->ratings[]=$rating;
+
+    public function receiveRating(float $rating)
+    {
+        $this->ratings[] = $rating;
     }
-    public function averageRating():float{
-        if(count($this->ratings)===0) return 0;
-        return array_sum($this->ratings)/count($this->ratings);
+
+    public function averageRating(): float
+    {
+        if (count($this->ratings) === 0) return 0;
+        return array_sum($this->ratings) / count($this->ratings);
     }
 }
 
-$app =new Application();
+
+$app = new Application();
 $app->run();
